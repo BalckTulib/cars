ba0b1220db3c500880bb8c7eafd7e4bc0bae239f
GHAZALI LOUBNA <loubna.ghazali16@gmail.com>
2024-01-07 22:36:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AboutComponent } from './component/about/about.component';
+import { FooterComponent } from './component/home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
